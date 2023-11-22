206235ac7934736eb356134208d77c472c5a74b6
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2023-11-21 20:56:53 -0500

    Move thread logic to seperate header

--- src/LoadingLayer.hpp
+++ src/LoadingLayer.hpp
@@ -1,18 +1,7 @@
 #pragma once
 
 #include "Node.hpp"
-
-#ifndef _WIN32
-#include <pthread.h>
-#define THREAD_TYPE pthread_t
-#define THREAD_CREATE(variable, function, args) pthread_create(&variable, NULL, function, args);
-#define THREAD_JOIN(variable) pthread_join(variable, NULL);
-#else
-#include <thread>
-#define THREAD_TYPE std::thread
-#define THREAD_CREATE(variable, function, args) variable = std::thread(function, args);
-#define THREAD_JOIN(variable) variable.join();
-#endif
+#include "Thread.hpp"
 
 #include "Texture.hpp"
 #include <tuple>
